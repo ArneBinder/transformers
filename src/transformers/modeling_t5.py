124c1c5ba278d6fa149c0ed5a86962ea1e2f12bc
Arne Binder <arne.binder@dfki.de>
2020-01-10 19:06:09 +0100

    add T5WithLMAndRPPHeadModel (language modeling + relative position prediction) stub

--- src/transformers/modeling_t5.py
+++ src/transformers/modeling_t5.py
@@ -33,7 +33,7 @@ from .modeling_utils import PreTrainedModel, prune_linear_layer
 
 logger = logging.getLogger(__name__)
 
-ENSURE_DEFAULT_RELATIVE_POSITION = False
+ENSURE_DEFAULT_RELATIVE_POSITION = True
 
 ####################################################
 # This dict contrains shortcut names and associated url
@@ -295,8 +295,9 @@ class T5Attention(nn.Module):
         """
         mask_special = None
         values_special = None
-        # TODO: this is not efficient because all values are embedded twice!
+
         # use separate embedding for special relative_positions (>= RELATIVE_POSITION_SPECIAL_OFFSET)
+        # TODO: this is not efficient because all values are embedded twice!
         if self.relative_attention_num_buckets_special > 0:
             mask_special = relative_position >= T5Attention.RELATIVE_POSITION_SPECIAL_OFFSET
             rp_bucket_special = torch.where(mask_special,
@@ -477,7 +478,7 @@ class T5Block(nn.Module):
         encoder_decoder_position_bias=None,
         head_mask=None,
         relative_position=None,
-        encoder_decoder_relative_position=None,
+        encoder_decoder_relative_position=None, # in fact, that are the decoder-to-encoder relative_positions
     ):
         self_attention_outputs = self.layer[0](
             hidden_states, attention_mask=attention_mask, position_bias=position_bias, head_mask=head_mask,
@@ -584,7 +585,7 @@ class T5Stack(T5PreTrainedModel):
         encoder_attention_mask=None,
         head_mask=None,
         relative_position=None,
-        encoder_decoder_relative_position=None
+        encoder_decoder_relative_position=None # in fact, that are the decoder-to-encoder relative_positions
     ):
         batch_size, seq_length = hidden_states.shape[0], hidden_states.shape[1]
         if attention_mask is None:
@@ -980,5 +981,165 @@ class T5WithLMHeadModel(T5PreTrainedModel):
             decoder_outputs = (
                 loss,
             ) + decoder_outputs  # TODO(thom): Add z_loss https://github.com/tensorflow/mesh/blob/fa19d69eafc9a482aff0b59ddd96b025c0cb207d/mesh_tensorflow/layers.py#L666
+
+        return decoder_outputs + encoder_outputs
+
+
+@add_start_docstrings("""T5 Model with a `language modeling` head and a `relative position prediction` head on top. """,
+                      T5_START_DOCSTRING, T5_INPUTS_DOCSTRING)
+class T5WithLMAndRPPHeadModel(T5PreTrainedModel):
+    r"""
+        **lm_labels**: (`optional`) ``torch.LongTensor`` of shape ``(batch_size, sequence_length)``:
+            Labels for computing the masked language modeling loss.
+            Indices should be in ``[-1, 0, ..., config.vocab_size]`` (see ``input_ids`` docstring)
+            Tokens with indices set to ``-1`` are ignored (masked), the loss is only computed for the tokens with labels
+            in ``[0, ..., config.vocab_size]``
+        **relative_position_labels**: (`optional`) ``torch.LongTensor`` of shape ``(batch_size, sequence_length,
+            sequence_length)``:
+
+    Outputs: `Tuple` comprising various elements depending on the configuration (config) and inputs:
+        **loss**: (`optional`, returned when ``lm_labels`` is provided) ``torch.FloatTensor`` of shape ``(1,)``:
+            Masked language modeling loss.
+        **prediction_scores**: ``torch.FloatTensor`` of shape ``(batch_size, sequence_length, config.vocab_size)``
+            Prediction scores of the language modeling head (scores for each vocabulary token before SoftMax).
+        **hidden_states**: (`optional`, returned when ``config.output_hidden_states=True``)
+            list of ``torch.FloatTensor`` (one for the output of each layer + the output of the embeddings)
+            of shape ``(batch_size, sequence_length, hidden_size)``:
+            Hidden-states of the model at the output of each layer plus the initial embedding outputs.
+        **attentions**: (`optional`, returned when ``config.output_attentions=True``)
+            list of ``torch.FloatTensor`` (one for each layer) of shape ``(batch_size, num_heads, sequence_length, sequence_length)``:
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention heads.
+
+    Examples::
+
+        tokenizer = T5Tokenizer.from_pretrained('t5-small')
+        model = T5WithLMHeadModel.from_pretrained('t5-small')
+        input_ids = torch.tensor(tokenizer.encode("Hello, my dog is cute")).unsqueeze(0)  # Batch size 1
+        outputs = model(input_ids=input_ids, lm_labels=input_ids)
+        loss, prediction_scores = outputs[:2]
+
+    """
+
+    def __init__(self, config):
+        super(T5WithLMAndRPPHeadModel, self).__init__(config)
+        config.relative_attention_num_buckets_special = 1   # for "infinite" relative_position
+
+        self.model_dim = config.d_model
+
+        self.shared = nn.Embedding(config.vocab_size, config.d_model)
+
+        encoder_config = copy.deepcopy(config)
+        self.encoder = T5Stack(encoder_config)
+
+        decoder_config = copy.deepcopy(config)
+        decoder_config.is_decoder = True
+        self.decoder = T5Stack(decoder_config)
+
+        self.lm_head = nn.Linear(config.d_model, config.vocab_size, bias=False)
+
+        self.encoder_relative_position_head = nn.Linear(config.d_model,
+                                                        config.relative_attention_num_buckets
+                                                        + config.relative_attention_num_buckets_special, bias=False)
+        self.decoder_relative_position_head = nn.Linear(config.d_model,
+                                                        config.relative_attention_num_buckets
+                                                        + config.relative_attention_num_buckets_special, bias=False)
+
+        self.init_weights()
+
+    def get_input_embeddings(self):
+        return self.shared
+
+    def set_input_embeddings(self, new_embeddings):
+        self.shared = new_embeddings
+
+    def get_output_embeddings(self):
+        return self.lm_head
+
+    def forward(self, **kwargs):
+        # keyword arguments come in 3 flavors: encoder-specific (prefixed by
+        # `encoder_`), decoder-specific (prefixed by `decoder_`) and those
+        # that apply to the model as whole.
+        # We let the specific kwargs override the common ones in case of conflict.
+
+        encoder_decoder_relative_position = kwargs.pop("encoder_decoder_relative_position", None)
+
+        lm_labels = kwargs.pop("decoder_lm_labels", None)
+
+        encoder_decoder_relative_position_labels = kwargs.pop("encoder_decoder_relative_position_labels", None)
+        decoder_relative_position_labels = kwargs.pop("decoder_relative_position_labels", None)
+
+        kwargs_common = dict(
+            (k, v) for k, v in kwargs.items() if not k.startswith("encoder_") and not k.startswith("decoder_")
+        )
+        kwargs_encoder = kwargs_common.copy()
+        kwargs_decoder = kwargs_common.copy()
+        kwargs_encoder.update(dict((k[len("encoder_") :], v) for k, v in kwargs.items() if k.startswith("encoder_")))
+        kwargs_decoder.update(dict((k[len("decoder_") :], v) for k, v in kwargs.items() if k.startswith("decoder_")))
+
+        # Encode if needed (training, first prediction pass)
+        encoder_hidden_states = kwargs_encoder.pop("hidden_states", None)
+        if encoder_hidden_states is None:
+            # Convert encoder inputs in embeddings if needed
+            hidden_states = kwargs_encoder.pop("inputs_embeds", None)
+            if hidden_states is None:
+                encoder_inputs_ids = kwargs_encoder.pop("input_ids")
+                hidden_states = self.shared(encoder_inputs_ids)  # Convert inputs in embeddings
+
+            encoder_outputs = self.encoder(hidden_states, **kwargs_encoder)
+            encoder_hidden_states = encoder_outputs[0]
+        else:
+            encoder_outputs = ()
+
+        encoder_next_relative_position_logits = self.encoder_relative_position_head(encoder_hidden_states)
+
+        # Decode
+        # Convert decoder inputs in embeddings if needed
+        hidden_states = kwargs_decoder.pop("inputs_embeds", None)
+        if hidden_states is None:
+            decoder_inputs_ids = kwargs_decoder.pop("input_ids")
+            hidden_states = self.shared(decoder_inputs_ids)
+
+        kwargs_decoder["encoder_hidden_states"] = encoder_hidden_states
+        kwargs_decoder["encoder_attention_mask"] = kwargs_encoder.get("attention_mask", None)
+        # decoder expects relative positions across stacks from its perspective, but encoder_decoder_relative_position
+        # contains relative positions from the perspective of the encoder, so we transpose
+        #if encoder_decoder_relative_position is not None:
+        kwargs_decoder["encoder_decoder_relative_position"] = encoder_decoder_relative_position #.transpose(-2,-1)
+        decoder_outputs = self.decoder(hidden_states, **kwargs_decoder)
+
+        decoder_next_relative_position_logits = self.decoder_relative_position_head(encoder_hidden_states)
+
+        sequence_output = decoder_outputs[0]
+        # Rescale output before projecting on vocab
+        # See https://github.com/tensorflow/mesh/blob/fa19d69eafc9a482aff0b59ddd96b025c0cb207d/mesh_tensorflow/transformer/transformer.py#L586
+        sequence_output = sequence_output * (self.model_dim ** -0.5)
+        lm_logits = self.lm_head(sequence_output)
+
+        decoder_outputs = (lm_logits,) + decoder_outputs[1:]  # Add hidden states and attention if they are here
+        if lm_labels is not None:
+            shift_logits = lm_logits[..., :-1, :].contiguous()
+            shift_labels = lm_labels[..., 1:].contiguous()
+            loss_fct = CrossEntropyLoss(ignore_index=-100)
+            loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
+            decoder_outputs = (
+                loss,
+            ) + decoder_outputs  # TODO(thom): Add z_loss https://github.com/tensorflow/mesh/blob/fa19d69eafc9a482aff0b59ddd96b025c0cb207d/mesh_tensorflow/layers.py#L666
+
+        next_relative_position_logits = decoder_next_relative_position_logits
+        relative_position_labels = decoder_relative_position_labels
+        # prepend encoder-to-decoder labels and logits
+        if encoder_decoder_relative_position_labels is not None:
+            assert decoder_relative_position_labels is not None, \
+                'decoder_relative_position_labels is required if encoder_decoder_relative_position_labels is given'
+            # TODO: re-work!
+            relative_position_labels = torch.cat((encoder_decoder_relative_position_labels,
+                                                  relative_position_labels), dim=1)
+            next_relative_position_logits = torch.cat((encoder_next_relative_position_logits,
+                                                       next_relative_position_logits), dim=1)
+
+        if decoder_relative_position_labels is not None:
+            raise NotImplementedError('implement loss calculation')
+            # calculate CrossEntropyLoss with respect to all target relative_distance rows, but return only best
+            # (minimal) loss
 
         return decoder_outputs + encoder_outputs
